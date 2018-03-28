cf1f0e7adb434e34358c6346bb92f5a82ac9c47b
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 11:19:07 +0200

    refactor

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -1,12 +1,24 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BoardShould {
+    Board board;
+
+    @Before
+    public void prepare_standard_boar(){
+        board = new Board(5,4);
+    }
+
     @Test
     public void be_of_given_size(){
-        Board board = new Board(3,4);
-        assertEquals(board.getWidth(),3);
+        assertEquals(board.getWidth(),5);
         assertEquals(board.getHeigh(),4);
+    }
+
+    @Test
+    public void get_is_alive_of_a_cell(){
+
     }
 }
