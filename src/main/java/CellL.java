db8b5892a506bbbe4f028bcab5e307659d692857
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 15:16:26 +0200

    failing test

--- src/main/java/CellL.java
+++ src/main/java/CellL.java
@@ -1,2 +1,11 @@
 public class CellL {
+    private boolean alive = false;
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive() {
+        alive = true;
+    }
 }
--- src/test/java/CellLShould.java
+++ src/test/java/CellLShould.java
@@ -1,3 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class CellLShould {
 
+    private CellAccessor cellAccessor;
+    private CellL cell;
+
+    @Before
+    public void before(){
+        cellAccessor = new CellAccessor();
+        cell = cellAccessor.getCell(new Coordinate(0,0));
+
+    }
+
+    @Test
+    public void be_dead_at_creation(){
+        CellL c = new CellL();
+        assertFalse(c.isAlive());
+    }
+
+    @Test
+    public void count_his_neighbours(){
+        assertEquals(0,cell.countNeigbours());
+
+        cellAccessor.getCell(new Coordinate(0,1)).setAlive();
+
+        assertEquals(0,cell.countNeigbours(),1);
+    }
 }
