8b7847e6cf085e84d430dc7b3b31c3eef1b3cad5
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 11:23:22 +0200

    failing test

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -2,6 +2,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class BoardShould {
     Board board;
@@ -20,9 +22,14 @@ public class BoardShould {
     @Test
     public void tart_with_all_cells_dead(){
         assertFalse(board.isAlive(0,1));
-        assertFalse(board.isAlive(0,2));
-        assertFalse(board.isAlive(0,3));
-        assertFalse(board.isAlive(0,4));
+        assertFalse(board.isAlive(1,2));
+        assertFalse(board.isAlive(2,3));
+        assertFalse(board.isAlive(3,4));
+    }
 
+    @Test
+    public void set_a_cell_to_alive(){
+        board.setAlive(1,2);
+        assertTrue(board.isAlive(1,2));
     }
 }
