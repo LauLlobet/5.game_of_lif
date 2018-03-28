296be90302dca0bbfc69458eb0f3bb909f6efb82
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 11:40:48 +0200

    passing test

---
+++ src/main/java/Board.java
@@ -0,0 +1,31 @@
+public class Board {
+    private boolean[][] array;
+    private int heigh;
+    private int width;
+
+    public Board(int width, int height) {
+        this.width = width;
+        this.heigh = height;
+        this.array = new boolean[width][height];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeigh() {
+        return heigh;
+    }
+
+    public boolean isAlive(int row, int column) {
+        return array[row][column];
+    }
+
+    public void setAlive(int row, int column) {
+        array[row][column] = true;
+    }
+
+    public int countAliveNeighbours(int row, int column) {
+        return  0;
+    }
+}
