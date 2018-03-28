a4dd09d60b8d2a132a2f8d418d5dd9841212c517
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 14:56:46 +0200

    test passes

--- src/main/java/CellAccessor.java
+++ src/main/java/CellAccessor.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+
 public class CellAccessor {
+    private HashMap<String,CellL> hashMap = new HashMap<String, CellL>();
     public CellL getCell(int i, int i1) {
-        return new CellL();
+        CellL guessedCell = hashMap.get(""+i+","+i1);
+        if(guessedCell != null){
+            return guessedCell;
+        }
+        hashMap.put(""+i+","+i1,new CellL());
+        return getCell(i,i1);
     }
 }
