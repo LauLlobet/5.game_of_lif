5b0b701eba21a84d51161292f916b98fbc178fdc
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 11:15:57 +0200

    be of a given size

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -1,8 +1,10 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BoardShould {
     @Test
-    public void be_of_random_size(){
+    public void be_of_given_size(){
         Board board = new Board(3,4);
         assertEquals(board.getWidth(),3);
         assertEquals(board.getHeigh(),4);
