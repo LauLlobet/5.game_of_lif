213697482fe1289c4db5821f438173e456c2af3d
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 12:19:58 +0200

    refactoring done

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,12 +1,18 @@
+
 public class Board {
-    private int[][] array;
+    private BoardCell[][] array;
     private int heigh;
     private int width;
 
     Board(int width, int height) {
         this.width = width;
         this.heigh = height;
-        this.array = new int[width][height];
+        this.array = new BoardCell[width][height];
+        for (int x = 0; x<width; x++){
+            for (int y =0; y<height; y++){
+                array[x][y] = BoardCell.DEAD;
+            }
+        }
     }
 
     public int getWidth() {
@@ -18,30 +24,30 @@ public class Board {
     }
 
     public boolean isAlive(int row, int column) {
-        return array[row][column] == 1;
+        return array[row][column] == BoardCell.ALIVE;
     }
 
     public void setAlive(int row, int column) {
-        array[row][column] = 1;
+        array[row][column] = BoardCell.ALIVE;
     }
 
     public int countAliveNeighbours(int row, int column) {
         int neighbours = 0;
         if(row > 0){
-            neighbours +=array[row-1][column];
-            neighbours +=array[row+1][column-1];
+            neighbours +=array[row-1][column].getIsAliveCount();
+            neighbours +=array[row+1][column-1].getIsAliveCount();
         }
         if(column > 0){
-            neighbours += array[row][column-1];
-            neighbours += array[row-1][column+1];
+            neighbours += array[row][column-1].getIsAliveCount();
+            neighbours += array[row-1][column+1].getIsAliveCount();
         }
         if (column > 0 && row > 0){
-            neighbours += array[row-1][column-1];
+            neighbours += array[row-1][column-1].getIsAliveCount();
 
         }
         return  neighbours +
-                array[row+1][column]+
-                array[row][column+1]+
-                array[row+1][column+1];
+                array[row+1][column].getIsAliveCount()+
+                array[row][column+1].getIsAliveCount()+
+                array[row+1][column+1].getIsAliveCount();
     }
 }
