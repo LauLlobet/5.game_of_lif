b853d392cb2c482ca0742a643e6715df636e4196
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 14:23:08 +0200

    failing test on board size

---
+++ src/test/java/Board.java
@@ -0,0 +1,16 @@
+public class Board {
+    private int width;
+    private int heigh;
+
+    public Board(int width, int height) {
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeigh() {
+        return heigh;
+    }
+}
