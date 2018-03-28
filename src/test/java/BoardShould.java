42a4e0c44f464dd369c42f9352092487f87ee275
Lau Llobet <lau.llobet@gmail.com>
2018-03-28 14:34:58 +0200

    test passes

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class BoardShould {
@@ -8,17 +10,18 @@ public class BoardShould {
 
     @Before
     public void prepare_standard_boar(){
-        board = new Board(5,4);
+        board = new Board(20,30);
     }
 
     @Test
     public void be_of_given_size(){
-        assertEquals(board.getWidth(),5);
-        assertEquals(board.getHeigh(),4);
+        assertEquals(board.getWidth(),20);
+        assertEquals(board.getHeigh(),30);
     }
 
     @Test
-    public void get_is_alive_of_a_cell(){
-
+    public  void create_cells_with_correct_neighbours(){
+        ArrayList<CellL> neighbours = board.getNeighbours(0,0);
+        assertEquals(8, neighbours.size());
     }
 }
